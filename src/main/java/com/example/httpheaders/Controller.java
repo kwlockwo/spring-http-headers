03b316c58b3b73004f8dc810b2397a2548f9fe3e
Keith Lockwood <kwlockwo@gmail.com>
2017-12-14 13:35:01 +1100

    request mapping ... again

--- src/main/java/com/example/httpheaders/Controller.java
+++ src/main/java/com/example/httpheaders/Controller.java
@@ -3,11 +3,13 @@ package com.example.httpheaders;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 	
+	@RequestMapping(value = "**")
 	public Map<String, String> headers(@RequestHeader Map<String, String> headers) {
 		return headers;
 	}
