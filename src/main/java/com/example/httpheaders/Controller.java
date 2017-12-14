f2771cce7c21b2fe1b2d8504064a2878084332d0
Keith Lockwood <kwlockwo@gmail.com>
2017-12-14 13:29:15 +1100

    remove imports

--- src/main/java/com/example/httpheaders/Controller.java
+++ src/main/java/com/example/httpheaders/Controller.java
@@ -3,8 +3,6 @@ package com.example.httpheaders;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
